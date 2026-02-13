bfc8a1295a04983225da0a7bd72d08fa7cdc7846
Jattyjardez <akanbasi7@gmail.com>
2026-02-13 10:57:31 +0000

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,44 @@
+let SName = "";
+let SNum = "";
+
+function greetUser(){
+  let name = document.getElementById("nameInput").value;
+  
+  let ans = confirm("Are you sure?");
+  
+  if(ans){
+    SName = name;
+    alert("Name saved! ");
+  }else{
+    alert("Submission canceled...");
+  }
+}
+
+
+
+function Num(){
+  let num = document.getElementById("numInput").value;
+  
+  let ans = confirm("Are you sure?");
+  
+  if(ans){
+    SNum = num;
+    alert("Number saved! ");
+  }else{
+    alert("Submission canceled...");
+  }
+}
+
+function show(){
+  let name = document.getElementById("nameInput").value;
+  let num = document.getElementById("numInput").value;
+  
+  
+  if(name==="" || num===""){
+    alert("Please fill all the required fields !!!");
+    return;
+  }
+  alert("Form details are as follows: \n Name: " + SName + "\n Number: " + SNum);
+      
+  alert("Thanks for your submission! \n Jardez Company LTD");
+}
