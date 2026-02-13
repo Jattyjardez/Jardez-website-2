9dd5a560306af46e2dfc51d1b37d2f8e8f13b2de
Jattyjardez <akanbasi7@gmail.com>
2026-02-13 15:59:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,45 +6,39 @@ function greetUser(){
   let ans = confirm("Are you sure?");
   
   if(ans){
-      if(name ===""){
-    alert("This field must not be empty. Pls try again! ");
+      if(name === ""){
+          alert("This field must not be empty. Pls try again!");
+      }else{
+          SName = name;   // FIXED
+          alert("Name saved!");
+      }
   }else{
-      SName = name;
-      alert("Name saved! ");
-  }
-}else{
-    alert("Submission canceled...");
+      alert("Submission canceled...");
   }
 }
-
-
 
 function Num(){
   let num = document.getElementById("numInput").value;
   let ans = confirm("Are you sure?");
   
-  
   if(ans){
-      if(num ===""){
-    alert("This field must not be empty. Pls try again! ");
+      if(num === ""){
+          alert("This field must not be empty. Pls try again!");
+      }else{
+          SNum = num;
+          alert("Number saved!");
+      }
   }else{
-      SNum = num;
-      alert("Number saved! ");
-  }
-}else{
-    alert("Submission canceled...");
+      alert("Submission canceled...");
   }
 }
+
 function show(){
-  let name = document.getElementById("nameInput").value;
-  let num = document.getElementById("numInput").value;
-  
-  
-  if(name==="" || num===""){
+  if(SName === "" || SNum === ""){
     alert("Please fill all the required fields !!!");
     return;
   }
-  alert("Form details are as follows: \n Name: " + SName + "\n Number: " + SNum);
-      
-  alert("Thanks for your submission! \n Jardez Company LTD🤝");
+
+  alert("Form details are as follows:\nName: " + SName + "\nNumber: " + SNum);
+  alert("Thanks for your submission!\nJardez Company LTD 🤝");
 }
